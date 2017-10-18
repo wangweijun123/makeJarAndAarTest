5e772adbba58729d29051a19efc99579df7e575b
wangweijun1 <wangweijun1@le.com>
2017-10-18 17:48:20 +0800

    test 调用aar

--- app/src/main/java/com/le/www/makejarandaartest/MainActivity.java
+++ app/src/main/java/com/le/www/makejarandaartest/MainActivity.java
@@ -2,6 +2,9 @@ package com.le.www.makejarandaartest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.le.www.toollibrary.ToolLibrary;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 使用module中的类
+        ToolLibrary.test();
+        // 使用module中资源
+        String s = getString(R.string.test);
+
+        Log.i("ToolLibrary", s);
     }
 }
